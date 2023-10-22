4b8cea2ec5a6ae690aa340d2a826628acba9bdfa
EvivNotrub <barthwerle@yahoo.de>
2023-10-23 00:50:51 +0200

    scss cleanup

--- frontend/client/src/containers/Form/Form.jsx
+++ frontend/client/src/containers/Form/Form.jsx
@@ -5,26 +5,17 @@ import './form.scss'
 function Form() {
 
         return (
-                <section className='log-in'>
-                    <div className='log-in__heading'>
-                        <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
-                                {/* <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
-                                    <path d="M399 384.2C376.9 345.8 335.4 320 288 320H224c-47.4 0-88.9 25.8-111 64.2c35.2 39.2 86.2 63.8 143 63.8s107.8-24.7 143-63.8zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm256 16a72 72 0 1 0 0-144 72 72 0 1 0 0 144z"/>
-                        </svg>
-                        <h2 className='log-in__heading__title'>Sign In</h2>
-                    </div>
-                    <form className='log-in__form' id='log-in-form'>
-                        <Field inputClass='input' labelClass='label bold' labelText='Username' type='text' inputName='name'/>
-                        <Field inputClass='input' labelClass='label bold' labelText='Password' type= 'password' inputName= 'password'/>
-                        <Field inputClass='log-in__form__remember' labelClass='label checkbox' labelTextAfter='Remember me' type='checkbox' id='rememberMe' inputName= 'rememberMe'/>
-                        <Button
-                            stateChange='log-in'
-                            className='log-in__form__submit button'
-                            type='submit'
-                            form='log-in-form'
-                            textContent='Sign In'/>
-                    </form>
-                </section>
+            <form className='form' id='log-in-form'>
+                <Field inputClass='input' labelClass='label bold' labelText='Username' type='text' inputName='name'/>
+                <Field inputClass='input' labelClass='label bold' labelText='Password' type= 'password' inputName= 'password'/>
+                <Field inputClass='form__remember' labelClass='label checkbox' labelTextAfter='Remember me' type='checkbox' id='rememberMe' inputName= 'rememberMe'/>
+                <Button
+                    stateChange='log-in'
+                    className='form__submit button'
+                    type='submit'
+                    form='log-in-form'
+                    textContent='Sign In'/>
+            </form>
         )
 }
 
