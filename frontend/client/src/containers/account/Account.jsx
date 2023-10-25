848ec8606f0b210b0de31c798365f47a26343d6c
EvivNotrub <barthwerle@yahoo.de>
2023-10-25 23:19:51 +0200

    userpage & accounts

--- frontend/client/src/containers/account/Account.jsx
+++ frontend/client/src/containers/account/Account.jsx
@@ -8,9 +8,9 @@ function Account({...props}){
     return (
         <section className={'account' + ' ' + props.className}>
             <div className='account__details'>
-                <h3 className="account__details__title">Argent Bank Checking (x8349)</h3>
-                <p className="account__details__amount">$2,082.79</p>
-                <p className="account__details__description">Available Balance</p>
+                <h3 className="account__details__title">{props.name}</h3>
+                <p className="account__details__amount">${props.balance}</p>
+                <p className="account__details__description">{props.balanceType}</p>
             </div>
             <div className='account__button--wrapper'>
                 <Button
@@ -25,6 +25,9 @@ function Account({...props}){
 
 Account.propTypes = {
     className: PropTypes.string,
+    name: PropTypes.string,
+    balance: PropTypes.string,
+    balanceType: PropTypes.string,
 }
 
 export default Account
--- frontend/client/src/pages/UserPage/UserPage.jsx
+++ frontend/client/src/pages/UserPage/UserPage.jsx
@@ -13,10 +13,25 @@ function UserPage() {
     const isAuthorized = useSelector((state) => state.user.isAuthorized);
     const hasToken = useSelector((state) => state.user.hasToken);
     const userData = useSelector((state) => state.user.userData);
-    const userName = useSelector((state) => state.user.user);
     const dispatch = useDispatch();
 
-
+    const accounts = [
+        {
+            "accountName": 'Argent Bank Checking (x8349)',
+            "balance": '2,082.79',
+            "balanceType": 'Available Balance',
+        },
+        {
+            "accountName": 'Argent Bank Savings (x6712)',
+            "balance": '10,928.42',
+            "balanceType": 'Available Balance',
+        },
+        {
+            "accountName": 'Argent Bank Credit Card (x8349)',
+            "balance": '184.30',
+            "balanceType": 'Current Balance',
+        },
+    ]
     const getUserData = useCallback(
         async () => {
                 const token = JSON.parse(localStorage.getItem("token"));
@@ -54,14 +69,17 @@ function UserPage() {
         return (
             <main className="user-main">
                 <div className="user-main__header">
-                    <h2>Welcome back<br/>{userName ? userName : ''} {userData ? userData.lastName : ''}!</h2>
+                    <h2>Welcome back<br/>{userData ? userData.firstName : ''} {userData ? userData.lastName : ''}!</h2>
                     <Button
                             // handleAction={() =>  }
                             type='button'
                             textContent='Edit Name'/>
                 </div>
                 <div className="user-main__accounts">
-                    <Account className='user-main__accounts__account' />
+                    {accounts.map((account) => (
+                        <Account key={account.balance + account.accountName} name={account.accountName} balance={account.balance} balanceType={account.balanceType} className='user-main__accounts__account' />
+                    ))
+                    }
                 </div>
             </main>
         )
