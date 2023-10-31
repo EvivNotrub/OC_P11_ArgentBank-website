e8a9a74b842780c48255ea7195c969304a74dddb
EvivNotrub <barthwerle@yahoo.de>
2023-10-31 19:23:56 +0100

    nvigation style

--- frontend/client/src/containers/Navigation/Navigation.jsx
+++ frontend/client/src/containers/Navigation/Navigation.jsx
@@ -37,7 +37,7 @@ function Navigation() {
     const signIn = 'sign-in';
     if(!isAuthorized){
         return (
-            <div>
+            <div className='header__nav__link-container'>
                 <Link className='header__nav__link' to={"/authentification/" + signIn}>
                     <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512">
                     {/* <!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --> */}
