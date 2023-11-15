a11d7e7614d78ab59543a176ec2388994d65004e
EvivNotrub <barthwerle@yahoo.de>
2023-11-15 23:49:16 +0100

    redux logger added

--- frontend/client/src/Redux/store.js
+++ frontend/client/src/Redux/store.js
@@ -3,12 +3,16 @@
 import { configureStore } from '@reduxjs/toolkit';
 import userReducer from './userSlice';
 import authReducer from './authSlice';
+import { createLogger } from 'redux-logger';
+
+const logger = createLogger();
 
 const store = configureStore({
   reducer: {
     user: userReducer,
     auth: authReducer,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
